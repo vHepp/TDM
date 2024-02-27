f6a1b99c5db7cf0169a89548c58829235aa1ed38
Vincent Hepola <vuhepola1227@gmail.com>
2024-02-27 01:24:32 -0500

    scripts

---
+++ model.py
@@ -0,0 +1,11 @@
+import torch
+from torchvision import models
+
+print("Building model")
+model = models.mobilenet_v2()
+
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
+model.load_state_dict(torch.load("model.pth"))
+
+print(model)
