0d52316a59188330fc158e356b0673dd8164c5fc
Vincent Hepola <vuhepola1227@gmail.com>
2024-04-27 13:53:21 -0400

    trim model output

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 import torch
 from torchvision import models
+from torchvision.models.mobilenetv2 import MobileNet_V2_Weights
 
 print("Building model")
-model = models.mobilenet_v2(weights="DEFAULT")
+model = models.mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
 
 torch.save(model.state_dict(), "model.pth")
 print("Saved PyTorch Model State to model.pth")
-model.load_state_dict(torch.load("model.pth"))
-
-print(model)
+# model.load_state_dict(torch.load("model.pth"))
+#
+# print(model)
--- quantized_model.py
+++ quantized_model.py
@@ -7,10 +7,10 @@ from torchvision.models.quantization import MobileNet_V2_QuantizedWeights
 torch.backends.quantized.engine = 'qnnpack'
 
 print("Building quantized model")
-model = models.quantization.mobilenet_v2(weights = "DEFAULT",quantize=True)
+model = models.quantization.mobilenet_v2(weights="DEFAULT", quantize=True)
 
 torch.save(model.state_dict(), "model_quantized.pth")
-print("Saved PyTorch Model State to model.pth")
-model.load_state_dict(torch.load("model_quantized.pth"))
+print("Saved PyTorch Model State to model_quantized.pth")
+# model.load_state_dict(torch.load("model_quantized.pth"))
 
-print(model)
+# print(model)
